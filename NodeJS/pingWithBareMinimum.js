87bc5bec891b15640bbe29aa53a61e6bf014fddd
David Gatti <office@gatti.pl>
2016-06-27 15:49:43 +0200

    Simplification

--- NodeJS/pingWithBareMinimum.js
+++ NodeJS/pingWithBareMinimum.js
@@ -1,15 +1,12 @@
 'use strict';
 
-var raw = require ("raw-socket");
-
-/**
- *
- * ICMP ECHO header
- *
- */
-
+let raw = require ("raw-socket");
 let header = [];
 
+//
+//	1. Create the structure where we are going to 
+//	   save our header
+//
 let type            = new Buffer(1);
 let code            = new Buffer(1);
 let identifier      = new Buffer(2);
@@ -17,6 +14,9 @@ let sequence_number = new Buffer(2);
 let chksum          = new Buffer(2);
 let data            = new Buffer(4);
 
+//
+//	2. Wrtie the apropriate values.
+//
 type.writeUInt8(0x8, 0);
 code.writeUInt8(0x0, 0);
 identifier.writeUInt16BE(0x0, 0);
@@ -24,6 +24,9 @@ sequence_number.writeUInt16BE(0x0, 0);
 chksum.writeUInt16BE(0xf7ff, 0);
 data.writeUInt32BE(0x0, 0);
 
+//
+//	3. push each separated buffer to the array
+//
 header.push(type);
 header.push(code);
 header.push(identifier);
@@ -31,35 +34,45 @@ header.push(sequence_number);
 header.push(chksum);
 header.push(data);
 
+//
+//	4. Combine all the buffers in to one
+//
 let headerConcat = new Buffer.concat(header, 12);
 
-/**
- *
- * Creatign the socket uign the ICMP protocol
- *
- */
+//
+//	5. Creatign the socket uign the ICMP protocol
+//
+var socket = raw.createSocket(
+	{
+		protocol: raw.Protocol.ICMP
+	}
+);
 
-var socket = raw.createSocket({protocol: raw.Protocol.ICMP});
+//
+//	6. Sendign the request for a ping
+//
+socket.send(headerConcat, 0, 12, "8.8.8.8", function(error, bytes)
+	{
+	    // 
+	    //	-> If there is any error, show it.
+	    // 
+	    if (error) 
+	    {
+	        console.log(error.toString());
+	    }
+	}
+);
 
-/**
- *
- * Sendign the request for a ping
- *
- */
-socket.send(headerConcat, 0, 12, "8.8.8.8", function(error, bytes) {
+//
+//	7. Listent for the remote host response
+//
+socket.on("message", function (buffer, source) 
+	{
+		//
+		//	-> Show the response message
+		//
+	    console.log ("Received " + buffer.length + " bytes from " + source);
 
-    // If there is any error, show it.
-    if (error) {
-        console.log(error.toString());
-    }
-
-});
-
-socket.on("message", function (buffer, source) {
-
-    console.log ("Received " + buffer.length + " bytes from " + source);
-
-    // Once we have our response we can exit the app
-    process.exit()
-
-});
+	    process.exit()
+	}
+);
