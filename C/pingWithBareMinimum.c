292cde36a5e3b4927e21b5fa4ebf92cc219685bc
David Gatti <office@gatti.pl>
2016-06-27 14:45:01 +0200

    Simplification

--- C/pingWithBareMinimum.c
+++ C/pingWithBareMinimum.c
@@ -13,40 +13,18 @@ int main() {
     //
     int s = socket(PF_INET, SOCK_RAW, 1);
 
-    if(s > 0) {
-
-        printf("Socket created\n");
-
-	} else {
-
+    //
+    //  -> Exit the app if the socket failed to be created
+    //
+    if(s <= 0)
+    {
         perror("Socket Error");
         exit(0);
-
     }
 
     //
-    // 2. Settign options
+    // 2. Create the ICMP Struct Header
     //
-    const int ttl = 30;
-
-    int actionSocketOptions = setsockopt(s, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
-
-    if(actionSocketOptions >= 0) {
-
-        printf("Options set\n");
-
-    } else {
-
-        perror("Options Error");
-        exit(0);
-
-    }
-
-    //
-    // 3. Sending PING
-    //
-
-    // ICMP Header
     typedef struct {
         uint8_t type;
         uint8_t code;
@@ -54,60 +32,73 @@ int main() {
         uint32_t data;
     } icmp_hdr_t;
 
+    //
+    //  3. Use the newly created struct to make a variable.
+    //
     icmp_hdr_t pckt;
 
-    pckt.type = 8;
-    pckt.code = 0;
-    pckt.chksum = 0xfff7;
-    pckt.data = 0;
+    //
+    //  4. Set the apropriate values to our struct, which is our ICMP header
+    //
+    pckt.type = 8;          // The echo request is 8
+    pckt.code = 0;          // No need
+    pckt.chksum = 0xfff7;   // Fixed checksume since the data is not changing
+    pckt.data = 0;          // We don't send anything.
 
-    // IP Header
+    //
+    //  5. Creatign a IP Header from a struct that exists in another library
+    //  
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = 0;
     addr.sin_addr.s_addr = inet_addr("8.8.8.8");
 
+    //
+    //  6. Send our PING 
+    //
     int actionSendResult = sendto(s, &pckt, sizeof(pckt), 0, (struct sockaddr*)&addr, sizeof(addr));
 
-    if(actionSendResult > 0) {
-
-        printf("Ping sent at %d bytes\n", actionSendResult);
-
-    } else {
-
+    //
+    //  -> Exit the app if the option failed to be set
+    //
+    if(actionSendResult < 0)
+    {
         perror("Ping Error");
         exit(0);
-
     }
 
     //
-    // 4. Readign the response
+    // 7. Prepare all the necesary variable to handle the response
     //
     unsigned int resAddressSize;
     unsigned char res[30] = "";
     struct sockaddr resAddress;
 
+    //
+    //  8. Read the response from the remote host
+    //
     int ressponse = recvfrom(s, res, sizeof(res), 0, &resAddress, &resAddressSize);
 
-    if( ressponse > 0) {
+    //
+    //  -> Display the response in its raw form (hex)
+    //
+    if( ressponse > 0)
+    {
+        printf("Message is %d bytes long, and looks like this:\n", ressponse);
 
-        printf("Message is %d bytes: ", ressponse);
-
-        for(int i = 0; i < ressponse; i++) {
-
+        for(int i = 0; i < ressponse; i++)
+        {
             printf("%x ", res[i]);
-
         }
 
         printf("\n");
 
         exit(0);
-
-    } else {
-
+    }
+    else 
+    {
         perror("Response Error");
         exit(0);
-
     }
 
     return 0;
